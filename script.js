939804f282031fc73098ec246f3c270de9daa487
Jack Neuner <Jneuner2165@gmail.com>
2023-03-15 22:47:08 -0600

    fixed styling so background bubble were no longer in the forground of webpage. Made all buttons and scales clickable, adjusted media player to be fixed in bottom left corner of browser. Javascript was updated to include click listener and take user to next page (still being built). All HTML was made 'prettier' so if other Devs look at code its easier to read

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 window.onload = function() {
-    const submitBtn = document.getElementById("submitBtn");
-    submitBtn.addEventListener("click", function(e) {
-      console.log("the submit button was clicked!");
-    });
+ const submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener("click", function(e) {
+  console.log("the submit button was clicked!");
   
-    const randomBtn = document.getElementById("randomBtn");
-    randomBtn.addEventListener("click", function(e) {
-      console.log("the random button was clicked!");
-    });
+  // Redirect to the new page
+  window.location.href = "https://www.example.com/new-page.html";
+});
+  
+const randomBtn = document.getElementById("randomBtn");
+randomBtn.addEventListener("click", function(e) {
+  console.log("the random button was clicked!");
+  
+  // Redirect to the new page
+  window.location.href = "https://www.example.com/new-page.html";
+});
   }
+  
